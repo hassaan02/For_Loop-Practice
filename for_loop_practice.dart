9f71a3b1593c50e600d8813c81332d37308e82e1
hassaan02 <136227511+hassaan02@users.noreply.github.com>
2023-07-29 20:53:06 +0500

    initial commit

---
+++ for_loop_practice.dart
@@ -0,0 +1,62 @@
+      //============== Program to Calculate the Sum of Numbers ==================
+
+      // void main(){
+      //   int n =10;
+      //   int sum =0;
+
+      //   for(int i=1; i<=n; i++){
+      //     sum += i;
+      // }
+      // print("The sum from 1 to $n is = $sum");
+      // }
+
+
+      //============= Program to Check Prime Numbers =====================
+
+
+
+// void main(){
+//         int num = 32;
+//         bool isPrime = true;
+
+//         for(int i =2; i <= num ~/2; i++ ){
+//           if(num % i == 0){
+//             bool isPrime = true;
+//             break;    
+//           }
+             
+//         }
+
+//         if(isPrime){
+//            print("$num is a prime number");  
+//         }
+//         else{
+//           print("$num is not a prime number");
+//         }
+//        }
+
+
+
+       //==================Program to Print Even and Odd Numbers from a List ============
+
+      //  void main(){
+      //   List<int> Numbers = [1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17,19];
+      //   List<int> evenNumbers =[];
+      //   List<int> oddNumbers = [];
+
+      //   for( int n in Numbers){
+      //       if(n % 2 ==0){
+      //         evenNumbers.add(n);
+      //       }
+      //       else{
+      //         oddNumbers.add(n);
+      //       }
+
+      //       print("Even numbers are $evenNumbers");
+      //       print("Odd numbers are $oddNumbers");
+      //   }
+        
+      //  }
+
+
+      // ===================== 
